52affa69d9b22b5ca8a63b24c5123f1f6d1bc0da
mehmet ali sezgin <49106964+mehmetalisezgin@users.noreply.github.com>
2024-04-12 19:36:32 +0200

    day

---
+++ src/test/java/day05/C13_PostRequestAutomateToken.java
@@ -0,0 +1,9 @@
+package day05;
+
+public class C13_PostRequestAutomateToken {
+    // Create a class with name Authentication in the utilities package
+    // When we use post method usually the url is asking username and password or token
+    // Token usually changes in 6-12-24 hours automatically
+    // thats why we need to automate it
+
+}
