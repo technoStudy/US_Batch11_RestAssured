910763786624022b032bbb143478fe4e9d3268fc
mehmet ali sezgin <49106964+mehmetalisezgin@users.noreply.github.com>
2024-04-12 21:22:37 +0200

    updated

--- src/test/java/day05/C15_PojoGetRequest.java
+++ src/test/java/day05/C15_PojoGetRequest.java
@@ -1,4 +1,18 @@
 package day05;
 
 public class C15_PojoGetRequest {
+    /*
+    POJO : Stands for Plain Old Java Object, In the context of using a REST API in Java.
+    a POJO refers to a simple Java Class that encapsulates data and provides getter, setter, toString()
+    methods for accessing and modifying data.
+    Why we are using POJO?
+    - Data Representation
+    - Serialization
+    - Encapsulation
+    - Compatibility
+     */
+
+
+
+
 }
---
+++ src/test/java/pojo/BookingDetails.java
@@ -0,0 +1,23 @@
+package pojo;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data // Generates getter, setter, toString()
+@NoArgsConstructor // Generate a no-arguments constructor
+@AllArgsConstructor // Generate an all-arguments constructor
+
+public class BookingDetails {
+    private String firstname ;
+    private String lastname ;
+    private int totalprice ;
+    private boolean depositpaid ;
+    private String additionalneeds ;
+    // for the inner json it is better to create different pojo class
+    private BookingDates bookingDates;
+
+
+
+}
